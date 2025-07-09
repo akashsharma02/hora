2d3209a38ae527991ebf5c2147a6497af1ced94f
Akash Sharma <akashsharma@cmu.edu>
2025-07-09 15:24:43 -0400

    Change deploy script to work with ros2

--- deploy.py
+++ deploy.py
@@ -14,28 +14,31 @@ import hydra
 
 from omegaconf import DictConfig, OmegaConf
 from hora.utils.misc import set_np_formatting, set_seed
+
 # noinspection PyUnresolvedReferences
 from hora.algo.deploy.deploy import HardwarePlayer
 
 
 # ---- OmegaConf & Hydra Config
 # Resolvers used in hydra configs (see https://omegaconf.readthedocs.io/en/2.1_branch/usage.html#resolvers)
-OmegaConf.register_new_resolver('eq', lambda x, y: x.lower() == y.lower())
-OmegaConf.register_new_resolver('contains', lambda x, y: x.lower() in y.lower())
-OmegaConf.register_new_resolver('if', lambda pred, a, b: a if pred else b)
+OmegaConf.register_new_resolver("eq", lambda x, y: x.lower() == y.lower())
+OmegaConf.register_new_resolver("contains", lambda x, y: x.lower() in y.lower())
+OmegaConf.register_new_resolver("if", lambda pred, a, b: a if pred else b)
 # allows us to resolve default arguments which are copied in multiple places in the config.
 # used primarily for num_ensv
-OmegaConf.register_new_resolver('resolve_default', lambda default, arg: default if arg == '' else arg)
+OmegaConf.register_new_resolver(
+    "resolve_default", lambda default, arg: default if arg == "" else arg
+)
 
 
-@hydra.main(config_name='config', config_path='configs')
+@hydra.main(config_name="config", config_path="configs")
 def main(config: DictConfig):
     set_np_formatting()
     config.seed = set_seed(config.seed)
     agent = HardwarePlayer(config)
     agent.restore(config.checkpoint)
-    agent.deploy()
+    agent.deploy(keyboard_interactive=config.keyboard_interactive)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- hora/algo/deploy/deploy.py
+++ hora/algo/deploy/deploy.py
@@ -4,7 +4,8 @@
 # Copyright (c) 2022 Haozhi Qi
 # Licensed under The MIT License [see LICENSE for details]
 # --------------------------------------------------------
-
+import threading
+import signal
 import numpy as np
 import os
 from hora.algo.models.models import ActorCritic
@@ -18,7 +19,9 @@ def _obs_allegro2hora(obses):
     obs_middle = obses[4:8]
     obs_ring = obses[8:12]
     obs_thumb = obses[12:16]
-    obses = np.concatenate([obs_index, obs_thumb, obs_middle, obs_ring]).astype(np.float32)
+    obses = np.concatenate([obs_index, obs_thumb, obs_middle, obs_ring]).astype(
+        np.float32
+    )
     return obses
 
 
@@ -34,17 +37,17 @@ class HardwarePlayer(object):
     def __init__(self, config):
         self.action_scale = 1 / 24
         self.actions_num = 16
-        self.device = 'cuda'
+        self.device = "cuda"
 
         obs_shape = (96,)
         net_config = {
-            'actions_num': self.actions_num,
-            'input_shape': obs_shape,
-            'actor_units': [512, 256, 128],
-            'priv_mlp_units': [256, 128, 8],
-            'priv_info': True,
-            'proprio_adapt': True,
-            'priv_info_dim': 9,
+            "actions_num": self.actions_num,
+            "input_shape": obs_shape,
+            "actor_units": [512, 256, 128],
+            "priv_mlp_units": [256, 128, 8],
+            "priv_info": True,
+            "proprio_adapt": True,
+            "priv_info_dim": 9,
         }
 
         self.model = ActorCritic(net_config)
@@ -56,53 +59,129 @@ class HardwarePlayer(object):
         self.sa_mean_std.eval()
         # hand setting
         self.init_pose = [
-            0.0627, 1.2923, 0.3383, 0.1088, 0.0724, 1.1983, 0.1551, 0.1499,
-            0.1343, 1.1736, 0.5355, 0.2164, 1.1202, 1.1374, 0.8535, -0.0852,
+            0.0627,
+            1.2923,
+            0.3383,
+            0.1088,
+            0.0724,
+            1.1983,
+            0.1551,
+            0.1499,
+            0.1343,
+            1.1736,
+            0.5355,
+            0.2164,
+            1.1202,
+            1.1374,
+            0.8535,
+            -0.0852,
         ]
-        self.allegro_dof_lower = torch.from_numpy(np.array([
-            -0.4700, -0.1960, -0.1740, -0.2270, 0.2630, -0.1050, -0.1890, -0.1620,
-            -0.4700, -0.1960, -0.1740, -0.2270, -0.4700, -0.1960, -0.1740, -0.2270
-        ])).to(self.device)
-        self.allegro_dof_upper = torch.from_numpy(np.array([
-            0.4700, 1.6100, 1.7090, 1.6180, 1.3960, 1.1630, 1.6440, 1.7190,
-            0.4700, 1.6100, 1.7090, 1.6180, 0.4700, 1.6100, 1.7090, 1.6180
-        ])).to(self.device)
+        self.allegro_dof_lower = torch.from_numpy(
+            np.array(
+                [
+                    -0.4700,
+                    -0.1960,
+                    -0.1740,
+                    -0.2270,
+                    0.2630,
+                    -0.1050,
+                    -0.1890,
+                    -0.1620,
+                    -0.4700,
+                    -0.1960,
+                    -0.1740,
+                    -0.2270,
+                    -0.4700,
+                    -0.1960,
+                    -0.1740,
+                    -0.2270,
+                ]
+            )
+        ).to(self.device)
+        self.allegro_dof_upper = torch.from_numpy(
+            np.array(
+                [
+                    0.4700,
+                    1.6100,
+                    1.7090,
+                    1.6180,
+                    1.3960,
+                    1.1630,
+                    1.6440,
+                    1.7190,
+                    0.4700,
+                    1.6100,
+                    1.7090,
+                    1.6180,
+                    0.4700,
+                    1.6100,
+                    1.7090,
+                    1.6180,
+                ]
+            )
+        ).to(self.device)
 
-    def deploy(self):
-        import rospy
-        from hora.algo.deploy.robots.allegro import Allegro
-        # try to set up rospy
-        rospy.init_node('example')
-        allegro = Allegro(hand_topic_prefix='allegroHand_0')
-        # Wait for connections.
-        rospy.sleep(0.5)
+    def deploy(self, keyboard_interactive=False):
+        import rclpy
+
+        # from hora.algo.deploy.robots.allegro import Allegro
+        from gum.devices.metahand.allegro_client import AllegroRobot
+
+        rclpy.init()
+        executor = rclpy.executors.SingleThreadedExecutor()
+        allegro = AllegroRobot()
+
+        def disconnect(signum, frame):
+            print("ctrl-C received")
+            allegro.disconnect()
+            sys.exit(-1)
+
+        signal.signal(signal.SIGINT, disconnect)
+
+        executor.add_node(allegro)
+
+        threading.Thread(target=executor.spin, daemon=True).start()
 
         hz = 20
-        ros_rate = rospy.Rate(hz)
+        ros_rate = allegro.create_rate(hz)
 
-        # command to the initial position
-        for t in range(hz * 4):
-            tprint(f'setup {t} / {hz * 4}')
-            allegro.command_joint_position(self.init_pose)
-            obses, _ = allegro.poll_joint_position(wait=True)
+        if keyboard_interactive:
+            input("press to move to initial position")
+
+        obses, _ = allegro.poll_joint_position(wait=True)
+        step = hz * 4
+        for i in range(step):
+            target_joints = i / step * np.array(self.init_pose) + (
+                1 - i / step
+            ) * np.array(obses)
+            allegro.command_joint_position(target_joints)
             ros_rate.sleep()
+
+        if keyboard_interactive:
+            input("press to deploy policy")
 
         obses, _ = allegro.poll_joint_position(wait=True)
         obses = _obs_allegro2hora(obses)
         # hardware deployment buffer
         obs_buf = torch.from_numpy(np.zeros((1, 16 * 3 * 2)).astype(np.float32)).cuda()
-        proprio_hist_buf = torch.from_numpy(np.zeros((1, 30, 16 * 2)).astype(np.float32)).cuda()
+        proprio_hist_buf = torch.from_numpy(
+            np.zeros((1, 30, 16 * 2)).astype(np.float32)
+        ).cuda()
 
         def unscale(x, lower, upper):
             return (2.0 * x - upper - lower) / (upper - lower)
 
         obses = torch.from_numpy(obses.astype(np.float32)).cuda()
         prev_target = obses[None].clone()
-        cur_obs_buf = unscale(obses, self.allegro_dof_lower, self.allegro_dof_upper)[None]
+        cur_obs_buf = unscale(obses, self.allegro_dof_lower, self.allegro_dof_upper)[
+            None
+        ]
 
         for i in range(3):
-            obs_buf[:, i*16+0:i*16+16] = cur_obs_buf.clone()  # joint position
-            obs_buf[:, i*16+16:i*16+32] = prev_target.clone()  # current target (obs_t-1 + s * act_t-1)
+            obs_buf[:, i * 16 + 0 : i * 16 + 16] = cur_obs_buf.clone()  # joint position
+            obs_buf[:, i * 16 + 16 : i * 16 + 32] = (
+                prev_target.clone()
+            )  # current target (obs_t-1 + s * act_t-1)
 
         proprio_hist_buf[:, :, :16] = cur_obs_buf.clone()
         proprio_hist_buf[:, :, 16:32] = prev_target.clone()
@@ -110,8 +189,8 @@ class HardwarePlayer(object):
         while True:
             obs = self.running_mean_std(obs_buf.clone())
             input_dict = {
-                'obs': obs,
-                'proprio_hist': self.sa_mean_std(proprio_hist_buf.clone()),
+                "obs": obs,
+                "proprio_hist": self.sa_mean_std(proprio_hist_buf.clone()),
             }
             action = self.model.act_inference(input_dict)
             action = torch.clamp(action, -1.0, 1.0)
@@ -129,20 +208,20 @@ class HardwarePlayer(object):
             obses = _obs_allegro2hora(obses)
             obses = torch.from_numpy(obses.astype(np.float32)).cuda()
 
-            cur_obs_buf = unscale(obses, self.allegro_dof_lower, self.allegro_dof_upper)[None]
+            cur_obs_buf = unscale(
+                obses, self.allegro_dof_lower, self.allegro_dof_upper
+            )[None]
             prev_obs_buf = obs_buf[:, 32:].clone()
             obs_buf[:, :64] = prev_obs_buf
             obs_buf[:, 64:80] = cur_obs_buf.clone()
             obs_buf[:, 80:96] = target.clone()
 
             priv_proprio_buf = proprio_hist_buf[:, 1:30, :].clone()
-            cur_proprio_buf = torch.cat([
-                cur_obs_buf, target.clone()
-            ], dim=-1)[:, None]
+            cur_proprio_buf = torch.cat([cur_obs_buf, target.clone()], dim=-1)[:, None]
             proprio_hist_buf[:] = torch.cat([priv_proprio_buf, cur_proprio_buf], dim=1)
 
     def restore(self, fn):
         checkpoint = torch.load(fn)
-        self.running_mean_std.load_state_dict(checkpoint['running_mean_std'])
-        self.model.load_state_dict(checkpoint['model'])
-        self.sa_mean_std.load_state_dict(checkpoint['sa_mean_std'])
+        self.running_mean_std.load_state_dict(checkpoint["running_mean_std"])
+        self.model.load_state_dict(checkpoint["model"])
+        self.sa_mean_std.load_state_dict(checkpoint["sa_mean_std"])
